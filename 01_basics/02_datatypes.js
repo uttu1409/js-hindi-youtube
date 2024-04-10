f99d8e84dcb13899e8466c2ea3546c525334a004
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-10 12:30:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3=3) //we are using node js not browser
+
+console.log(3+3); //code readibility should be high
+
+let name = "Utsav"
+let age = 21
+let isLoggedIn = false
+
+// datatypes - premitive
+// number => 2 to power 53
+// bigint 
+// string ==> ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => to define uniqueness
+
+// objects - 
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
