2de4dfe65a85dc108eac6cd4a18095266ebb6e10
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-12 07:10:46 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,26 +1,67 @@
-let score = true
+// let score = true
 
 // const {score} = req.body // in this we don't know about the type of score
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true 0 => false
 // "" => false
 // "utsav" => true
 
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof someNumber);
 console.log(typeof stringNumber);
+*/
+
+// ******************************** Operations **********************************//
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2-2);
+//console.log(2**3);
+//console.log(2%3);
+//console.log(2/2);
+
+/*
+let str1 = "hello"
+let str2 = "utsav"
+let str3 = str1+ str2
+console.log(str3);
+*/
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
