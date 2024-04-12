86f14478e9f324ba60740de4a332931ae44437c2
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-12 06:56:25 +0000

    conversion to string

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,3 +15,12 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+// "utsav" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
