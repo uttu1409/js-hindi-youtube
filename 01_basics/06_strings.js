6d5a49d6e40bea1e563289cf4003ce3856c163dc
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-15 10:55:43 +0000

    string1

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,21 @@
+// "hello" + "world"
+
+const name = "Utsav"
+const repoCount = 50
+
+// console.log(name +  " " + repoCount + "Value");
+
+// console.log('Hello my name is ${name} and my repo is ${repoCount}'); // string interpulation - to do many things on the go
+
+console.log("Hello");
+
+const gameName = new String('Utsav ji')
+console.log(gameName[1]);
+console.log(gameName.__proto__); // to check objects 
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4)); // 4th position p konsa character hai
+// console.log(gameName.indexOf('s')); // 's' konse index pr hai
+
