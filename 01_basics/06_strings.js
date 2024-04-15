4cc13d3a09d846b3cb35c51ba8f46de0cdce6660
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-15 11:56:43 +0000

    string2

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -9,7 +9,7 @@ const repoCount = 50
 
 console.log("Hello");
 
-const gameName = new String('Utsav ji')
+const gameName = new String('Ut-sa-v ji')
 console.log(gameName[1]);
 console.log(gameName.__proto__); // to check objects 
 
@@ -18,4 +18,21 @@ console.log(gameName.__proto__); // to check objects
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(4)); // 4th position p konsa character hai
 // console.log(gameName.indexOf('s')); // 's' konse index pr hai
+
+const newString = gameName.substring(0,4) // last vala entry nhi lega 
+console.log(newString);
+
+const anotherString = gameName.slice(-7,4)
+console.log(anotherString);
+
+const newStringOne = "     Utsav.    "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // unnecessary space ko hata deta hai 
+
+const url = "https://utsav.com/utsav%20gupta"
+console.log(url.replace('%20', '-')); // keywords ko replace krne k liye
+
+console.log(url.includes('utsav')); // check krne k liye whether ek keyword isme include hai ya nahi
+
+console.log(gameName.split('-')); // kisi condition k basis p split krna
 
