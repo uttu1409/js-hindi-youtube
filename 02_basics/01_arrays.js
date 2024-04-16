65eab9b22611b982e4a1324ee6e019b565933685
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-16 13:03:08 +0000

    array1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// arays in JS are resizeable
+
+const myArr = [1,2,3,4,5]
+console.log(myArr[2]);
+
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr[3]);
+
+// Arrays Mathods
+myArr.push(0) // element insert krne k liye
+console.log(myArr);
+myArr.pop() // to remove the last element
+console.log(myArr);
