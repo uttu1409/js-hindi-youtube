44fa88599ccd2fb3962ed69fce337db47043d48b
Utsav Gupta <87597047+uttu1409@users.noreply.github.com>
2024-04-16 12:33:10 +0000

    dateNtime2

--- 01_basics/08_dateNtime.js
+++ 01_basics/08_dateNtime.js
@@ -13,5 +13,14 @@ const newDate = new Date(20, 0 , 23)
 console.log(newDate.toDateString());
 console.log(newDate.toLocaleString());
 
-const createdDate = new Date("2023-01-04")
+let createdDate = new Date("2023-01-04")
 console.log(createdDate.toLocaleString());
+
+let timeStamp = Date.now() // gives seconds from 1 Jan 1970
+console.log(timeStamp);
+console.log(createdDate.getTime());
+console.log(createdDate.getMonth()); // to get month 
+
+createdDate.toLocaleString('default', {
+    weekday: 'long'
+})
